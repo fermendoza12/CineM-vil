130d9bfccfe21ffa75c484e93fc7994e4c7f89b7
fermendoza12 <fercmendoza6@gmail.com>
2025-07-10 17:06:59 -0300

    Update data.js

--- data.js
+++ data.js
@@ -3,8 +3,7 @@ const peliculas = [
 id: "el-hombre-lobo-1941",
 titulo: "El hombre lobo (1941)",
 imagen: "https://image.tmdb.org/t/p/original/dXM0qoqk7SmDzSyRHCn3Hc08CKw.jpg",
-trailer: "https://www.youtube.com/watch?v=trailer2",
-enlace: "https://drive.google.com/...",
-tags: ["hombre lobo", "the wolf man", "lobos", "1941", "terror clásico"]
+enlace: "https://drive.google.com/file/d/1TebRrVdwIaT7GTOhU2n701PeZwGMP4gR/view?usp=drive_link",
+tags: ["el hombre lobo", "the wolf man", "1941", "fantastico", "monstruos", "licantropia", "cine clasico", "blanco y negro", "universal monster", "gotico"]
 }
 ];
